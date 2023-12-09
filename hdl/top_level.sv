0647dfaef02e329ff53212b48c078fa47666f182
Theo <theo.acooper@gmail.com>
2023-12-09 14:50:56 -0500

    started hardware intf

--- hdl/top_level.sv
+++ hdl/top_level.sv
@@ -14,15 +14,29 @@ module top_level(
   logic sys_rst;
   assign sys_rst = btn[0];
 
-  always_ff @(posedge clk_100mhz) begin
-  end
+  logic [7:0] byte_in;
+  logic in_trigger;
+  logic prev_in_trigger;
+  logic all_in_ready;
+  logic prev_all_in_ready;
 
-  // manta intf (
-  //   .clk(clk_100mhz),
-  //   .tx(uart_txd),
-  //   .rx(uart_rxd)
-  //   //...
-  // );
+  logic [7:0] byte_out;
+  logic out_trigger;
+  logic all_out_ready;
+  manta manta_inst (
+    .clk(clk_100mhz),
+    .rx(uart_rxd),
+    .tx(uart_txd),
+    .byte_in(byte_in),
+    .in_trigger(in_trigger),
+    .all_in_ready(all_in_ready),
+    .byte_out(byte_out),
+    .out_trigger(out_trigger),
+    .all_out_read(all_out_ready));
+
+  always_ff @(posedge clk_100mhz) begin
+    
+  end
 
 endmodule // top_level
 
